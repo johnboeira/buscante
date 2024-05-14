2bceff55b5181111552920117fe39476f6febe59
João <johnboeira@gmail.com>
2024-05-14 19:25:14 -0300

    feat : Usa operador map para transformar tipos

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,6 +1,7 @@
+import { Item } from './../models/interfaces';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { LivrosResultado } from '../models/interfaces';
 
 @Injectable({
@@ -11,10 +12,12 @@ export class LivroService {
   private readonly API = 'https://www.googleapis.com/books/v1/volumes'
   constructor(private http: HttpClient) { }
 
-  buscar(valorDigitado: string): Observable<LivrosResultado> {
+  buscar(valorDigitado: string): Observable<Item[]> {
     const params = new HttpParams().append('q', valorDigitado )
     return this.http.get<LivrosResultado>(this.API, { params }).pipe(
-      tap((retorno: any) => console.log(retorno))
+      tap((retorno: any) => console.log(retorno)),
+      map(retorno => retorno.items),
+      tap((retorno: any) => console.log('retorno pós map', retorno)),
     )
   }
 }
